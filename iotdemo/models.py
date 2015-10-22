e2940d75ca1dd3543c464c527b312c9bc62946c1
SkinnyRat <howard@flickps.com>
2015-10-22 13:51:30 +0800

    Update models.py

--- iotdemo/models.py
+++ iotdemo/models.py
@@ -16,6 +16,7 @@ class Cake(models.Model):
   label = models.CharField(max_length=128)
   price = models.IntegerField(default=0)
   quantity = models.IntegerField(default=0)
+  deviceid = models.CharField(max_length=128) 
   
   def __str__(self):
     return self.label
@@ -32,5 +33,4 @@ class Order(models.Model):
 # Cakes are added/managed in the main Admin panel by the store owner. 
 # Currently does not group together Orders with different Cakes but in the same checkout. 
 # Currently also does not allow image uploads for the different Cakes. 
-
 
