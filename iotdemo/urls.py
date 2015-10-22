98ff2724adcce5c4c7f52e48932026abda9af27f
SkinnyRat <howard@flickps.com>
2015-10-22 13:58:59 +0800

    Update urls.py

--- iotdemo/urls.py
+++ iotdemo/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
 	url(r'^cart/', views.CartView.as_view(), name='cart'),
 	url(r'^addtocart/', views.addtocart, name='addtocart'),
 	url(r'^checkout/', views.checkout, name='checkout'),
+	url(r'^price/(?P<device_id>\w+)/$', views.price, name='price'),
+	url(r'^update/(?P<add_sub>\w+)/(?P<device_id>\w+)/$', views.update, name='update'),
 ] 
 
 
